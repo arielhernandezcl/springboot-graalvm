eb359bb3d9bb2ae924881cb9c17ceee185d04171
arielhernandezcl <ariel.dox@gmail.com>
2024-04-02 01:53:48 -0300

    initial commit

---
+++ src/main/java/net/arielhernandez/springbootgraalvm/MainController.java
@@ -0,0 +1,13 @@
+package net.arielhernandez.springbootgraalvm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/net/arielhernandez/springbootgraalvm/SpringbootgraalvmApplication.java
@@ -0,0 +1,13 @@
+package net.arielhernandez.springbootgraalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgraalvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgraalvmApplication.class, args);
+	}
+
+}
